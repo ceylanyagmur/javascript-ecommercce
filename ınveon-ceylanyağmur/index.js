53e2200062951a1be1bf31c5279090ef13c18fa0
ceylan <92316535+ceylanyagmur@users.noreply.github.com>
2023-02-17 22:14:21 +0300

    Add files via upload

---
+++ ınveon-ceylanyağmur/index.js
@@ -0,0 +1,12 @@
+
+
+function displayColumn() {
+    document.querySelector('.displayRow').style.display = 'none'
+    document.querySelector('.displayColumn').style.display = 'block'
+}
+
+function displayRow() {
+    document.querySelector('.displayRow').style.display = 'block'
+    document.querySelector('.displayColumn').style.display = 'none'
+}
+
